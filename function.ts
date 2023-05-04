41955607dff2bb4406e5a64219d71f803f35b4c7
Matt Law <matthew.law@titanbay.com>
2023-05-04 13:42:16 +0100

    cut down

--- function.ts
+++
@@ -1,8 +0,0 @@
-import { http } from '@google-cloud/functions-framework'
-import { PubSub } from '@google-cloud/pubsub'
-
-http('entryPoint', async (req, res) => {
-  console.log('oh')
-
-  res.send(200)
-})
